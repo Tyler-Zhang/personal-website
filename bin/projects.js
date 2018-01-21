ab1fd556e991fe459acc35100bfee8bbba09efd0
Tyler Zhang <tztylerzhang@gmail.com>
2018-01-21 00:20:10 -0500

    projects section

---
+++ bin/projects.js
@@ -0,0 +1,20 @@
+const projects = [{
+  title: "Waterloop Goose2 Dashboard",
+  description: "Primary control panel for receiving hyperloop pod data and sending manual override commands.",
+  github: "https://github.com/teamwaterloop/goose2-dashboard"
+},{
+  title: "Waterloop Team Manager",
+  description: "Web app to help manage team members and automatically grant permissions to shared resources like Slack and Google Drive",
+  github: "https://github.com/teamwaterloop/team-manager-front"
+},{
+  title: "Screen Tick",
+  description: "ScreenTick is a device usage tracking webapp that helps parents monitor their childrens phone usage.",
+  image: "images/screentick-thumbnail.png",
+  link: "https://screentick.tylerzhang.com",
+  defaultLink: "https://screentick.tylerzhang.com"
+}, {
+  title: "Waterloo Quest +",
+  description: "Injects scraped professor ratings into school scheduling website to help students choose the best classes",
+  link: "https://chrome.google.com/webstore/detail/waterloo-quest%2B/flibldkpfnhllecdhoaogigdnajngaoc?hl=en",
+  github: "https://github.com/Tyler-Zhang/Waterloo-Quest-Plus"
+}]
