d2b82488d8ee57c0973531d191c0bf3673627475
Tyler Zhang <TZTylerzhang@gmail.com>
2018-01-23 18:14:51 -0500

    play video on hover, pause on hoveroff

--- bin/projects.js
+++ bin/projects.js
@@ -8,18 +8,21 @@ const projects = [{
 },{
   title: "Waterloop Team Manager",
   thumbnail: "images/team-manager-thumbnail.png",
+  // thumbnailVideo: "images/team-manager-video.mp4",
   description: "Web app that helps team leads at Waterloop manage their members. Logging in to the web portal allows new recruits to be easily assigned to roles and given permissions to team resources based on those roles.",
   github: "https://github.com/teamwaterloop/team-manager-front"
 },{
   title: "Screen Tick",
   description: "ScreenTick is a device usage tracking webapp that helps parents monitor their childrens' phone usage.",
   thumbnail: "images/screentick-thumbnail.png",
+  thumbnailVideo: "images/screentick-video.mp4",
   link: "https://screentick.tylerzhang.com",
   defaultLink: "https://screentick.tylerzhang.com"
 }, {
   title: "Waterloo Quest +",
   description: "Injects scraped professor ratings into University of Waterloo class scheduling website to help students choose the best professors.",
   thumbnail: "images/waterloo-quest-plus-thumbnail.png",
+  thumbnailVideo: "images/waterloo-quest-plus-video.mp4",
   link: "https://chrome.google.com/webstore/detail/waterloo-quest%2B/flibldkpfnhllecdhoaogigdnajngaoc?hl=en",
   github: "https://github.com/Tyler-Zhang/Waterloo-Quest-Plus"
 }]
