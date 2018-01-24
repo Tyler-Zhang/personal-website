7d00036df2730449b704392c1e2ae071eaf59b22
Tyler Zhang <tztylerzhang@gmail.com>
2018-01-24 01:21:29 -0500

    add behind the scenes sections for screen tick and waterloo quest +

--- bin/projects.js
+++ bin/projects.js
@@ -1,4 +1,20 @@
 const projects = [{
+  title: "Screen Tick",
+  description: "ScreenTick is a device usage tracking webapp that helps parents monitor their childrens' phone usage.",
+  thumbnail: "images/screentick-thumbnail.png",
+  thumbnailVideo: "images/screentick-video.mp4",
+  link: "https://screentick.tylerzhang.com",
+  defaultLink: "screentick.html",
+  info: "screentick.html"
+}, {
+  title: "Waterloo Quest +",
+  description: "Injects scraped professor ratings into University of Waterloo class scheduling website to help students choose the best professors.",
+  thumbnail: "images/waterloo-quest-plus-thumbnail.png",
+  thumbnailVideo: "images/waterloo-quest-plus-video.mp4",
+  link: "https://chrome.google.com/webstore/detail/waterloo-quest%2B/flibldkpfnhllecdhoaogigdnajngaoc?hl=en",
+  github: "https://github.com/Tyler-Zhang/Waterloo-Quest-Plus",
+  info: "wqp.html"
+}, {
   title: "Waterloop Goose2 Dashboard",
   description: "Primary control panel for the University of Waterloo's Hyperloop team: Waterloop. The dashboard is responsible for displaying real-time sensor data and making sure the pod is in a healthy state.",
   thumbnail: "images/dashboard-thumbnail.png",
@@ -8,21 +24,6 @@ const projects = [{
 },{
   title: "Waterloop Team Manager",
   thumbnail: "images/team-manager-thumbnail.png",
-  // thumbnailVideo: "images/team-manager-video.mp4",
-  description: "Web app that helps team leads at Waterloop manage their members. Logging in to the web portal allows new recruits to be easily assigned to roles and given permissions to team resources based on those roles.",
+  description: "Web app that helps team leads at Waterloop manage their members. Logging in to the web portal allows new recruits to be easily assigned to roles and given permissions to team resources based on those roles. (WIP)",
   github: "https://github.com/teamwaterloop/team-manager-front"
-},{
-  title: "Screen Tick",
-  description: "ScreenTick is a device usage tracking webapp that helps parents monitor their childrens' phone usage.",
-  thumbnail: "images/screentick-thumbnail.png",
-  thumbnailVideo: "images/screentick-video.mp4",
-  link: "https://screentick.tylerzhang.com",
-  defaultLink: "https://screentick.tylerzhang.com"
-}, {
-  title: "Waterloo Quest +",
-  description: "Injects scraped professor ratings into University of Waterloo class scheduling website to help students choose the best professors.",
-  thumbnail: "images/waterloo-quest-plus-thumbnail.png",
-  thumbnailVideo: "images/waterloo-quest-plus-video.mp4",
-  link: "https://chrome.google.com/webstore/detail/waterloo-quest%2B/flibldkpfnhllecdhoaogigdnajngaoc?hl=en",
-  github: "https://github.com/Tyler-Zhang/Waterloo-Quest-Plus"
 }]
